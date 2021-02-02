bfee81d14c48e405d39bee378efbaf5a072ea78c
Jorge Taramón <jorgetaramon@gmail.com>
2021-02-02 18:56:16 +0000

    Update txt2gpml.sh

--- springmesh_3d_spherical_shell/GPLATES/txt2gpml.sh
+++ springmesh_3d_spherical_shell/GPLATES/txt2gpml.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-cd /Users/jorge/Downloads/Mesh_Generator-master/springmesh_3d_spherical_shell/GPLATES/
 export PYTHONPATH=$PYTHONPATH://Users/jorge/Downloads/Mesh_Generator-master/springmesh_3d_spherical_shell/GPLATES/pygplates
 python PyGPlates_MeshNodePointsFromLatLonFile.py surf_nodes.txt points.gpml
